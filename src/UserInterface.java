37faadf5627bfd6eccc5447f7941903f42ebd4e4
taccat3 <tcaplan172@gmail.com>
2022-01-21 16:35:37 -0500

    recreating GUI

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,121 +1,59 @@
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.util.LinkedList;
 import java.util.List;
-
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTable;
-import javax.swing.JTextArea;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
-
-import BreezySwing.GBFrame;
-import BreezySwing.IntegerField;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.Color;
+import javax.swing.*;
+import BreezySwing.*;
 
 public class UserInterface extends GBFrame implements KeyListener {
 
 	private LinkedList<JComponent> sizeGUI;
 	private LinkedList<JComponent> rcGUI;
-
-	private JTable board;
+	private LinkedList<JComponent> boardGUI;
 
 	private int height;
 	private int width;
 
 	public UserInterface() {
-		sizeGUI = new LinkedList<JComponent>();
-		rcGUI = new LinkedList<JComponent>();
+		initSizeGUI();
+		
 
-		sizeGUI.add(addButton("Set Size", 3, 2, 1, 1));
-		sizeGUI.add(addLabel("Height: ", 1, 1, 1, 1));
-		sizeGUI.add(addIntegerField(0, 1, 2, 1, 1));
-		sizeGUI.add(addLabel("Width: ", 2, 1, 1, 1));
-		sizeGUI.add(addIntegerField(0, 2, 2, 1, 1));
-
-		rcGUI.add(addButton("Done", 4, 1, 1, 1));
-		// server.process("1\n1 2\n1 2 3\n1 2 3 4");
+		height = 0;
+		width = 0;
 	}
 
 	public static void main(String[] args) {
-		
-		int[][] columns5_1 = {{2, 2},
-						{5},
-						{0},
-						{0},
-						{0}};
-		int[][] rows5_1 = {{2},
-						   {2},
-						   {1},
-						   {2},
-						   {2}};
-		int[][] columns5_2 = {{2, 2},
-							{5},
-							{0},
-							{0},
-							{0}};
-		int[][] rows5_2 = {{2},
-						{2},
-						{1},
-						{2},
-						{2}};
-
-		int[][] columns5_3 = {{0},
-						   {0},
-						   {0},
-						   {5},
-						   {2, 2}};
-		int[][] rows5_3 = {{2},
-					    {2},
-					    {1},
-					    {2},
-					    {2}};
-		
-		int[][] rows5_4 = {{0},
-							{0},
-							{0},
-							{5},
-							{2, 2}};
-		int[][] columns5_4 = {{2},
-			    		{2},
-			    		{1},
-			    		{2},
-			    		{2}};
-
-		int[][] rows5_5 = {{3},
-							{2},
-							{2},
-							{2,1},
-							{3}};
-
-		int[][] columns5_5 = {{2},
-							{1,2},
-							{1,1},
-							{3},
-							{3}};
-
-
-
-		
-		
-		BoardSolver board2 = new BoardSolver(rows5_5, columns5_5);
-		// BoardSolver board2 = new BoardSolver(rows5_1, columns5_1);
-		
-		board2.solve();
-		
-//		JFrame frm = new UserInterface();
-//		frm.setTitle("Nonogram Solver");
-//		frm.setSize(500, 200);
-//		frm.setVisible(true);	
+		JFrame frm = new UserInterface();
+		frm.setTitle("Nonogram Solver");
+		frm.setSize(500, 200);
+		frm.setVisible(true);
 	}
 
 	public void buttonClicked(JButton btn) {
 		if (btn == sizeGUI.get(0)) {
+			IntegerField fldHeight = (IntegerField)(sizeGUI.get(1));
+			IntegerField fldWidth = (IntegerField)(sizeGUI.get(2));
+			if(fldHeight.isValidNumber() && fldWidth.isValidNumber()) {
+				height = fldHeight.getNumber();
+				width = fldWidth.getNumber();
+				hide(sizeGUI);
+				initRCGUI();
+			} else {
+				messageBox("Invalid Input try again");
+				if(!fldHeight.isValidNumber()) {
+					fldHeight.select(0, fldHeight.getText().length());
+					fldHeight.requestFocus();
+				}
+				if(!fldWidth.isValidNumber()) {
+					fldWidth.select(0, fldWidth.getText().length());
+					fldWidth.requestFocus();
+				}
+			}
 			
 		} else if (btn == rcGUI.get(0)) {
-			
+			hide(rcGUI);
+			initBoardGUI();
 		}
 	}
 
@@ -124,23 +62,50 @@ public class UserInterface extends GBFrame implements KeyListener {
 			part.setVisible(false);
 		}
 	}
+	private void show(List<JComponent> thing) {
+		for (JComponent part : thing) {
+			part.setVisible(true);
+		}
+	}
+
+	private void initSizeGUI() {
+		sizeGUI = new LinkedList<JComponent>();
+
+		sizeGUI.add(addButton("Set Size", 3, 2, 1, 1));
+		sizeGUI.add(addIntegerField(0, 1, 2, 1, 1)); // Height Field
+		sizeGUI.add(addIntegerField(0, 2, 2, 1, 1)); // Width Field
+		sizeGUI.add(addLabel("Height: ", 1, 1, 1, 1));
+		sizeGUI.add(addLabel("Width: ", 2, 1, 1, 1));
+	}
+
+	private void initRCGUI() {
+		rcGUI = new LinkedList<JComponent>();
+
+		rcGUI.add(addButton("Done", 4, 1, 1, 1));
+	}
+
+	private void initBoardGUI() {
+		boardGUI = new LinkedList<JComponent>();
+
+		boardGUI.add(addTextArea("", 1, 1, 5, 5));
+		((JTextArea)boardGUI.get(0)).setEditable(false);
+	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
-
 }
